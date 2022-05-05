81109b847464b6ea3d3660c2f73b1b1d7ad988be
Dhanfinix <66616896+Dhanfinix@users.noreply.github.com>
2022-05-05 10:08:27 +0700

    Lengkapi data partikel

--- app/src/main/java/com/dhandev/eepa/addition/dataActivity.kt
+++ app/src/main/java/com/dhandev/eepa/addition/dataActivity.kt
@@ -19,6 +19,9 @@ class dataActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDataBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.arrowBack.setOnClickListener {
+            onBackPressed()
+        }
 
         val list = listOf("Partikel Elementer", "Partikel Komposit")
         adapter1(list)
@@ -31,15 +34,17 @@ class dataActivity : AppCompatActivity() {
 
         menuJenisDropdown?.setOnItemClickListener{parent, v, position, id ->
             val jenisTerpilih = adapter.getItem(position)
-            val listElementer = listOf("Boson", "Fermion", "Graviton")
+            val listElementer = listOf("Boson", "Fermion")
             val listKomposit = listOf("Baryons", "Mesons")
             when(jenisTerpilih){
                 "Partikel Elementer" -> {
                     binding.menuJenis2.editText?.setText("Pilih jenis partikel")
+                    binding.menuJenis2.hint = "Jenis Distribusi Partikel"
                     adapter2(listElementer)
                 }
                 "Partikel Komposit" -> {
                     binding.menuJenis2.editText?.setText("Pilih jenis distribusi")
+                    binding.menuJenis2.hint = "Keluarga Hadron"
                     adapter2(listKomposit)
                 }
             }
@@ -54,8 +59,14 @@ class dataActivity : AppCompatActivity() {
         menuDistribusiDropdown?.setOnItemClickListener{parent, v, position, id ->
             val jenisTerpilih = adapter2.getItem(position)
             val listBoson = listOf("Foton", "W Boson", "Z Boson", "Gluon", "Higgs Boson")
+            val listFermion = listOf("Up", "Down", "Charm", "Strange", "Top", "Bottom", "Elektron", "Elektron Neutrino", "Muon", "Muon Neutrino", "Tau", "Tau Neutrino")
+            val listBaryon = listOf("Proton", "Neutron", "Lambda", "Sigma", "Xi", "Omega-minus")
             when(jenisTerpilih){
-                "Boson" -> adapter3(listBoson)
+                //partikel boson
+                "Boson" -> adapter3(listBoson)      //jika adapter2 kita plilih boson, maka adapter 3 akan menampilkan listBoson
+                "Fermion" -> adapter3(listFermion)
+                //partikel baryon
+                "Baryons" -> adapter3(listBaryon)
             }
         }
     }
@@ -67,9 +78,13 @@ class dataActivity : AppCompatActivity() {
 
         menuNamaDropdown?.setOnItemClickListener{parent, v, position, id ->
             val jenisTerpilih = adapter3.getItem(position)
-            val listFoton = listOf("Boson", "Foton", "Y", "0", "Stabil", "0", "1", "Pembawa gaya elektromagnetik", "1905 oleh Albert Einstein")
+            val listFoton = listOf("Boson", "Foton", "γ", "0", "Stabil", "0", "1", "Pembawa gaya elektromagnetik", "Albert Einstein (1905)\nNama \"foton\" umumnya dikaitkan dengan Gilbert N. Lewis (1926)")
+            val listUp = listOf("Quark", "Up", "u", "1,8-3,0 Mev", "Variabel", "+2/3", "1/2", "Penyusun partikel komposit", "Teori pada tahun 1964 oleh Gell-Mann dan Zweig.\nObservasi pada 1968-72 saat hamburan elektron di SLAC dan hamburan neutrino di CERN")
+            val listProton = listOf("Baryon", "Proton", "p", "938,3 Mev", "Stabil", "+1", "1/2", "Penyusun inti atom yang bermuatan positif", "1911-1919 oleh E.Rutherford")
             when(jenisTerpilih){
                 "Foton" -> detailData(listFoton)
+                "Proton" -> detailData(listProton)
+                "Up" -> detailData(listUp)
             }
         }
     }
